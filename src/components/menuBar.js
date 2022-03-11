d7e62316791810636fc75c7e02af544574d8ef2c
anakarina1928 <anakarina.nak@gmail.com>
2022-03-11 06:13:10 -0500

    refactorizacion de funciones

--- src/components/menuBar.js
+++ src/components/menuBar.js
@@ -1,4 +1,33 @@
+//import { logut } from "../firebase/auth.js"
+/*export const btnsign = (user)  => {
+let btnSignOut = document.getElementById(user)
+btnSignOut.addEventListener("click",async () => {
+    try{
+         let functionLogut= await logut()
+         sessionStorage.clear();
+        window.location.hash = '#/';
+         return functionLogut
+   }catch{
+       console.log("no se sale")
+   }    
+})
+}*/
+/*
+const cerrarSesion = () => {
+    const btnCerrarSesion = document.getElementById('cierre');
+    btnCerrarSesion.addEventListener('click', () => {
+        try {
+            let functionLogut = await logut()
+            sessionStorage.clear();
+            window.location.hash = '#/login';
+            return functionLogut
+        } catch {
+            console.log("no se sale")
+        }
+    });
+};*/
 export const menuBar = () => {
+    
     const templateMenuBar = `
     <div id="icons-menu" class="icons-menu">
         <div id="div-logo divs-menu" class="div-logo">
@@ -17,9 +46,10 @@ export const menuBar = () => {
             <i class="fa-solid fa-gear fafa-menu"></i>
         </span>
         <span class="divs-menu" >
-            <i class="fa-solid fa-arrow-right-to-bracket fafa-menu"></i>
+            <i  id ="cierre" class="fa-solid fa-arrow-right-to-bracket fafa-menu"></i>
         </span>
     </div>
         `
+    
     return templateMenuBar
 }
